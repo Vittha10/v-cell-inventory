d3eee49c40634bb9d59f6e6825b1c53942a0ae2d
Vita <vita.vita@itbss.ac.odgit config --global user.email vita.vita@itbss.ac,id>
2026-01-03 17:31:45 +0700

    First Commit

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                &copy; by V-CELL {{ date('Y') }}
+            </div>
+        </div>
+    </div>
+</footer>
